5c6332925ffd20c9750c9b8a2b37b3d7e38e1fd3
Sumeet Yeola <sumeetyeolapict@gmail.com>
2023-05-26 01:16:46 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Carousel
+/* label the images, just for convenience, to visually track them */
+let i = 1;
+for(let li of carousel.querySelectorAll('li')) {
+  li.style.position = 'relative';
+  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+  i++;
+}
+
+/* configuration */
+let width = 85; // image width
+let count = 1; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+// shift left
+position += width * count;
+// can't move to the left too much, end of images
+position = Math.min(position, 0)
+list.style.marginLeft = position + 'vw';
+};
+
+carousel.querySelector('.next').onclick = function() {
+// shift right
+position -= width * count;
+// can only shift the ribbbon for (total ribbon length - visible count) images
+position = Math.max(position, -width * (listElems.length - count));
+list.style.marginLeft = position + 'vw';
+};
+
+//Hamburger Menu
+const toggleButton = document.querySelector('.toggle-btn');
+const navbarLinks = document.querySelector('.nav-links');
+console.log(toggleButton,navbarLinks);
+toggleButton.addEventListener('click',() => {
+  console.log('clicked');
+  navbarLinks.classList.toggle('active');
+})
